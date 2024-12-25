e59d4d9cc841376e91e2cd79a6695938edab1c20
Michael Litvinov <mihaillitvinov26324@gmail.com>
2024-12-25 12:40:10 +0300

    add pass randomizer

--- src/dataset_generator/generator.py
+++ src/dataset_generator/generator.py
@@ -1,6 +1,7 @@
 import os
 import configparser
 import subprocess
+import itertools
 
 import pandas as pd
 
@@ -21,28 +22,40 @@ class Generator:
 
     def generate_files(self):
         for file in self._get_files():
-            out_asm_file, out_passes_asm_file, ret_code = self._gen_files(file)
-            if ret_code == 0:
-                self.out_files.append((out_asm_file, out_passes_asm_file, file))
+            pass_list = self.config.get('PASSES', 'pass_list').split('\n')[1:]
+            
+            score = {}
+            for perm_list in itertools.permutations(pass_list):    
+                passes = self._get_passes(perm_list)
 
+                out_asm_file, out_passes_asm_file, ret_code = self._gen_files(file, passes)
+                if ret_code == 0:
+                    out_asm_file_cnt = 0
+                    with open(out_asm_file) as f:
+                        out_asm_file_cnt = sum(1 for _ in f)
+                    
+                    out_passes_asm_file_cnt = 0
+                    with open(out_passes_asm_file) as f:
+                        out_passes_asm_file_cnt = sum(1 for _ in f)
+                    
+                    profit = (out_asm_file_cnt - out_passes_asm_file_cnt) / out_asm_file_cnt
+
+                    score[passes] = profit
+            
+            optimal_passes, optimal_score = list(score.items())[0]
+            for passes, score in score.items():
+                if score >= optimal_score:
+                    optimal_passes, optimal_score = passes, score
+            
+            print(file, optimal_passes, optimal_score)
+            self.out_files.append((file, optimal_passes))
 
     def make_dataset(self):
         data = []
         
-        for out_asm_file, out_passes_asm_file, cpp_file in self.out_files:
+        for cpp_file, optimal_passes in self.out_files:
             row = []
             row.append(cpp_file)
-            
-            out_asm_file_cnt = 0
-            with open(out_asm_file) as f:
-                out_asm_file_cnt = sum(1 for _ in f)
-            
-            out_passes_asm_file_cnt = 0
-            with open(out_passes_asm_file) as f:
-                out_passes_asm_file_cnt = sum(1 for _ in f)
-            
-            profit = (out_asm_file_cnt - out_passes_asm_file_cnt) / out_asm_file_cnt
-            print(profit, cpp_file)
             
             graph = ASTGraph(cpp_file)
             
@@ -63,7 +76,8 @@ class Generator:
                 # for param in embed[0]:
                 #     row.append(param)
             
-            row.append("some target")
+            row.append(optimal_passes)
+
             data.append(row)
             
         cols = [f'feature_{i}' for i in range(len(data[0]) - 2)]
@@ -99,7 +113,7 @@ class Generator:
         return filtered_list
         
 
-    def _gen_files(self, in_file):
+    def _gen_files(self, in_file, passes):
         components, ir_file = self._get_components_for_get_ir(in_file)
         cp = subprocess.run(components, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if cp.returncode != 0:
@@ -112,7 +126,7 @@ class Generator:
         
         self._drop_optnone(ir_file)
         
-        components, out_pass_file = self._get_components_for_custom_gen(ir_file)
+        components, out_pass_file = self._get_components_for_custom_gen(ir_file, passes)
         cp = subprocess.run(components, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if cp.returncode != 0:
             return "", "", cp.returncode
@@ -136,12 +150,10 @@ class Generator:
         return [program_name, in_file, flag_asm, flag_llvm, opt_flag, flag_name, out_file], out_file
         
 
-    def _get_components_for_custom_gen(self, in_file):
+    def _get_components_for_custom_gen(self, in_file, passes):
         program_name = 'opt'
         flag_asm = '-S'
         flag_passes = '-passes'
-        # passes = "default<Oz>"
-        passes = self._get_passes()
         flag_name = '-o'
         file_name = get_name_without_extention(in_file)
         out_file = file_name + 'll-passes'
@@ -158,10 +170,9 @@ class Generator:
         return [program_name, in_file, flag_name, out_file], out_file
 
 
-    def _get_passes(self):
+    def _get_passes(self, pass_list):
         passes = ''
         
-        pass_list = self.config.get('PASSES', 'pass_list').split('\n')[1:]
         for i, p in enumerate(pass_list):
             if i == 0:
                 passes += f'{actions[p].value}'   
