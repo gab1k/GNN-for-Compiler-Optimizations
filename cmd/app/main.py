edab5209c791a927bd3144cb3fb731162ff41b88
gab1k <gabidullinkamil1@gmail.com>
2024-12-25 14:21:26 +0300

    add model

--- cmd/app/main.py
+++ cmd/app/main.py
@@ -3,6 +3,11 @@ import configparser
 from app.app import App
 from src.model.model import Model
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import balanced_accuracy_score
+
 def get_config(config_path):
     config = configparser.ConfigParser()
     config.read(config_path)
@@ -16,14 +21,19 @@ if __name__=='__main__':
 
     config = get_config('configs/dataset_generator_config.ini')
 
-    data = load_iris()
-    X = pd.DataFrame(data.data, columns=data.feature_names)
-    y = pd.Series(data.target)
+    data = pd.read_csv('dataset.csv')
+    feature_columns = [f'feature_{i}' for i in range(1, 37)]
 
+    label_encoder = LabelEncoder()
+    data['target'] = label_encoder.fit_transform(data['target'])
+
+    X = data[feature_columns]
+    y = data['target']
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # Тут достаем свои 
     
-    catboost_model = Model(model_type="catboost", input_dim=4, num_clusses=3) 
-    pytorch_model = Model(model_type="pytorch", input_dim=4, num_clusses=3)
+    catboost_model = Model(model_type="catboost", input_dim=36, num_clusses=3) 
+    pytorch_model = Model(model_type="pytorch", input_dim=36, num_clusses=3)
 
     catboost_model.load()
     pytorch_model.load()
--- cmd/model/main.py
+++ cmd/model/main.py
@@ -1,5 +1,6 @@
 from src.model.model import Model
 import configparser
+from sklearn.preprocessing import LabelEncoder
 
 def get_config(config_path):
     config = configparser.ConfigParser()
@@ -8,17 +9,20 @@ def get_config(config_path):
     return config
 
 if __name__== '__main__':
-    from sklearn.datasets import load_iris
     import pandas as pd
-    from sklearn.metrics import balanced_accuracy_score
     from sklearn.model_selection import train_test_split
 
     config = get_config('configs/dataset_generator_config.ini')
 
-    data = load_iris()
-    X = pd.DataFrame(data.data, columns=data.feature_names)
-    y = pd.Series(data.target)
+    data = pd.read_csv('dataset.csv')
+    feature_columns = [f'feature_{i}' for i in range(1, 37)]
 
+    label_encoder = LabelEncoder()
+    data['target'] = label_encoder.fit_transform(data['target'])
+
+    X = data[feature_columns]
+    y = data['target']
+    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # Тут достаем свои 
 
     catboost_model = Model(model_type="catboost", input_dim=4, num_clusses=3) 
